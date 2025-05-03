72e71627a15e0f5d0b47c931c831bdf9c9b0e697
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2025-05-03 10:07:50 +0530

    port change

--- next.config.mjs
+++ next.config.mjs
@@ -22,15 +22,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Ignore ESLint errors during builds
   },
-
-  // Add custom port configuration
-  async rewrites() {
-    return [];
-  },
-  // Set custom port
-  serverOptions: {
-    port: 4000, // Change this number to your desired port
-  },
 };
 
 export default nextConfig;
